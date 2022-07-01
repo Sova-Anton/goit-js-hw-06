09be983960c5a69aaa377f43f80c8a92f588481e
Sova-Anton <anton.sova.goit@gmail.com>
2022-07-01 14:29:37 +0300

    task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+// const list = document.querySelector('#categories');
+// console.log(`Number of categories:`, list.children.length);
+
+const itemEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemEl.length}`);
+
+itemEl.forEach((element) => {
+    return console.log(`Category: ${element.querySelector("h2").textContent}
+Elements:${element.querySelectorAll("li").length}`);
+})
